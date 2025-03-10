8ee4f16023f092c5fdcdcfdc24cf0df6d93dbb49
PC_ALEX\alexp <alexpt6561@gmail.com>
2025-03-10 19:11:26 +0000

    primeira vez a tentar fazer alguma coisa de bom

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
-from flask import Flask
+import psycopg2
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+OK_CODE = 200
 @app.route('/')
-def home():
+def hello_world():
     return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+def db_connection():
+    credentials = "host= aid.estgoh.ipc.pt dbname=db20221090306 port=5432 user=a2022109306  password=a2022109306"
+    return psycopg2.connect(credentials)
+
+if __name__ == '__main__':
+    app.run()
