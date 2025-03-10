562aafba7d70704cfef67b2f3e2bc0728fd268e9
PC_ALEX\alexp <alexpt6561@gmail.com>
2025-03-10 20:55:44 +0000

    terceira vez a tentar

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 OK_CODE = 200
 @app.route('/')
 def hello_world():
-    return 'LUME'
+    return 'LUME!'
 
 def db_connection():
     credentials = "host= aid.estgoh.ipc.pt dbname=db20221090306 port=5432 user=a2022109306  password=a2022109306"
