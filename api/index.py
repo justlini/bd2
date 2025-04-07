02982f7daf810d433906e70924e975890c21f491
PC_ALEX\alexp <alexpt6561@gmail.com>
2025-04-07 20:52:33 +0100

    oitava vez a tentar

--- api/index.py
+++ api/index.py
@@ -14,6 +14,11 @@ def db_connection():
     if (connection):{
         print ("Connection established")
     }
-
+def db_connection():
+    credentials = "host= aid.estgoh.ipc.pt dbname=db20221090306 port=5432 user=a2022109306  password=a2022109306"
+    return psycopg2.connect(credentials)
+    if (connection):{
+        print ("Connection established")
+    }
 if __name__ == '__main__':
     app.run()
