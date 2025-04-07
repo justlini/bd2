7a20f5f42e08ddd1fc5734399985e5c1f6190853
Miguel <miguelafonsoplacido@gmail.com>
2025-04-07 19:39:23 +0100

    novo conn

--- api/conn.py
+++ api/conn.py
@@ -2,11 +2,14 @@
 import psycopg2
 import os
 
+
 def get_db_connection():
+    host = os.getenv("db_host")
     connection = psycopg2.connect(
-        host=os.getenv("db_host"),
-        database = os.getenv("db_database"),
-        user= os.getenv("db_user"),
-        password = os.getenv("db_password")
+        host=host,
+        database=os.getenv("db_database"),
+        user=os.getenv("db_user"),
+        password=os.getenv("db_password")
     )
+    print(host)  # Print host after it has been retrieved
     return connection
