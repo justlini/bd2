6013982c5bbb84ead5116c624b32b8b6accc0ead
justlini <150179544+justlini@users.noreply.github.com>
2025-04-07 21:02:05 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,24 @@
+import os
 import psycopg2
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 OK_CODE = 200
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/env')
+def print_env_vars():
+    env_vars = {
+        "db_host": os.getenv("db_host"),
+        "db_database": os.getenv("db_database"),
+        "db_user": os.getenv("db_user"),
+        "db_password": os.getenv("db_password")
+    }
+    return jsonify(env_vars), OK_CODE
 
 def get_db_connection():
     host = os.getenv("db_host")
@@ -16,8 +28,9 @@ def get_db_connection():
         user=os.getenv("db_user"),
         password=os.getenv("db_password")
     )
-    print(host)  #w Print host after it has been retrieved
+    print(f"Connected to DB at host: {host}")
     return connection
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
+
