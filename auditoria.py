909ab10d572642b005a7b6ecb84c87d666c20651
m11234 <miguelafonsoplacido@gmail.com>
2025-05-06 12:41:22 +0100

    faltava

--- auditoria.py
+++ auditoria.py
@@ -5,7 +5,7 @@ class ManageAuditoria:
     def __init__(self):
         self.bd = BaseDeDados()
 
-    def insert_Log(self, p_utilizador_bd, p_utilizador_app, p_acao):
+    def insert_Log(self, p_utilizador_bd, p_utilizador_app,p_data, p_acao):
         conn = self.bd.get_conn()
         if conn is None:
             return "Erro de conexÃ£o com a base de dados."
