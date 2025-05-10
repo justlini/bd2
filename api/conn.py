fd4e767899b15f8d40c2e185f1e82cc9552d2563
m11234 <miguelafonsoplacido@gmail.com>
2025-05-10 15:51:22 +0100

    maybe vai dar

--- api/conn.py
+++ api/conn.py
@@ -1,35 +1,43 @@
 import psycopg2
 import os
 
+import psycopg2
+import os
+
 class BaseDeDados:
-    def __init__(self, user=None, password=None):
-        #self.host = os.getenv("db_host", "localhost")
-        #self.database = os.getenv("db_database", "bd2")
-        #self.user = os.getenv("db_user", "postgres")
-        #self.password = os.getenv("db_password", "postgres")
-        #dados server
+    def __init__(self, user=None, password=None, user_type=None):
         self.host = os.getenv("db_host")
         self.database = os.getenv("db_database")
-        self.user = user if user else os.getenv("db_user")  # Use provided user or default
-        self.password = password if password else os.getenv("db_password")  # Use provided password or default
+
+        # if para login
+        if user_type == "admin":
+            self.user = os.getenv("admin_db_user")
+            self.password = os.getenv("admin_db_password")
+        elif user_type == "cliente":
+            self.user = os.getenv("cliente_db_user")
+            self.password = os.getenv("cliente_db_password")
+        elif user_type == "rececionista":
+            self.user = os.getenv("rececionista_db_user")
+            self.password = os.getenv("rececionista_db_password")
+
         print(f"Conectando ao banco de dados {self.database} no host {self.host} com o usuário {self.user}.")
 
     def get_conn(self):
-        # Verifica se todas as variáveis de ambiente existem
         if not all([self.host, self.database, self.user, self.password]):
-            return None  # Retorna None se faltar alguma variável
+            print("Missing database connection parameters.")
+            return None
 
         try:
-            # Conecta à base de dados
             connection = psycopg2.connect(
                 host=self.host,
                 database=self.database,
                 user=self.user,
                 password=self.password
             )
+            print("Database connection successful.")
             return connection
         except Exception as e:
-            print(f"Erro ao coneceetar à base de dados: {str(e)}")
+            print(f"Erro ao conectar à base de dados: {str(e)}")
             return None
         
     
