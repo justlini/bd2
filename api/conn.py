a00e15aa2fd0773a4f8fa05b2103d0ea861b8eef
m11234 <miguelafonsoplacido@gmail.com>
2025-05-10 15:38:43 +0100

    nova

--- api/conn.py
+++ api/conn.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 
 class BaseDeDados:
-    def __init__(self):
+    def __init__(self, user=None, password=None):
         #self.host = os.getenv("db_host", "localhost")
         #self.database = os.getenv("db_database", "bd2")
         #self.user = os.getenv("db_user", "postgres")
@@ -10,7 +10,7 @@ class BaseDeDados:
         #dados server
         self.host = os.getenv("db_host")
         self.database = os.getenv("db_database")
-        self.user = user if user else os.getenv("admim_db_user")  # Use provided user or default
+        self.user = user if user else os.getenv("admin_db_user")  # Use provided user or default
         self.password = password if password else os.getenv("db_password")  # Use provided password or default
         print(f"Conectando ao banco de dados {self.database} no host {self.host} com o usuário {self.user}.")
 
