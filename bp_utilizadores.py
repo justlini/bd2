b939cfc6bd658eb87fa3abc5b5f83094b0405f41
m11234 <miguelafonsoplacido@gmail.com>
2025-05-13 11:06:22 +0100

    identacaofix

--- bp_utilizadores.py
+++ bp_utilizadores.py
@@ -68,13 +68,15 @@ def registar_emp():
             data['tipoemp'],
             data['idcliente']
         )
-        log_message = f"Conta empregado criada: {data['idemp']}"
+        log_message = f"Conta empregado criada: {id}"
 
         message = manageAuditoria.insert_Log(p_utilizador_bd,p_utilizador_app,p_dataLog,log_message)
         
         #empregado existe
         if "Empregado inserido com sucesso!" in message:
             logging.info("Empregado inserido com sucesso!")
+            log_message = f"Conta empregado criada: {data['idemp']}"
+            message = manageAuditoria.insert_Log(p_utilizador_bd,p_utilizador_app,p_dataLog,log_message)
 
             #mensagem sucess no postman
             return jsonify({"message": message}), CREATED
