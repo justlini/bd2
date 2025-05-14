2bbe239afeddfea461fc30273f94eb8b26814f66
m11234 <miguelafonsoplacido@gmail.com>
2025-05-14 23:07:34 +0100

    getty ver pagamentos cliente

--- bp_transacoes.py
+++ bp_transacoes.py
@@ -48,11 +48,10 @@ def ver_pagamentos():
         return jsonify({"error": "Internal Server Error"}), INTERNAL_SERVER_ERROR
 
 
-@transacoes_bp.route('/ver_pagamentos_cliente', methods=['POST'])
+@transacoes_bp.route('/ver_pagamentos_cliente/<int:p_idcliente>', methods=['GET'])
 @jwt_required()
-def ver_pagamentos_cliente():
+def ver_pagamentos_cliente(p_idcliente):
     try:
-        data = request.get_json()
         user = get_jwt_identity()
         p_utilizador_app = user['nome']
         p_utilizador_bd = user['db_user']
@@ -62,24 +61,17 @@ def ver_pagamentos_cliente():
             logging.error("Unauthorized access attempt.")
             return jsonify({"error": "Unauthorized"}), BAD_REQUEST
         
-        if user['tipo'] == 'cliente' and data['p_idcliente'] != user['idcliente']:
+        if user['tipo'] == 'cliente' and p_idcliente != user['idcliente']:
             logging.error("Unauthorized access attempt.")
             return jsonify({"error": "Unauthorized"}), BAD_REQUEST
 
-        # Validar os parâmetros de entrada
-        if not all(k in data for k in ["p_idcliente"]):
-            logging.error("Faltam parametros!")
-            return jsonify({"error": "Faltam parametros!"}), BAD_REQUEST
-
         # Chamar a função para ver os pagamentos do cliente
-        pagamentos = manageTransacoes.verpagamentos_cliente(
-            data['p_idcliente']
-        )
+        pagamentos = manageTransacoes.verpagamentos_cliente(p_idcliente)
 
         if pagamentos:
             logging.info("Pagamentos do cliente obtidos com sucesso!")
-            log_message = f"Viu pagamentos do cliente com ID: {data['p_idcliente']}"
-            manageAuditoria.insert_Log(p_utilizador_bd,p_utilizador_app,p_dataLog,log_message)
+            log_message = f"Viu pagamentos do cliente com ID: {p_idcliente}"
+            manageAuditoria.insert_Log(p_utilizador_bd, p_utilizador_app, p_dataLog, log_message)
             return jsonify({"pagamentos": pagamentos}), OK_CODE
         else:
             logging.error("Erro ao obter pagamentos do cliente.")
@@ -87,4 +79,3 @@ def ver_pagamentos_cliente():
     except Exception as e:
         logging.error(f"Unexpected error: {str(e)}")
         return jsonify({"error": "Internal Server Error"}), INTERNAL_SERVER_ERROR
-    
